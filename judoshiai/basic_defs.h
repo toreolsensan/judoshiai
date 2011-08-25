2fc10a378bea9d65f6164a4b0517868812a48f8f
Hannu Jokinen <oh2ncp@kolumbus.fi>
2011-08-25 05:46:40 +0000

    Parser definitions.

---
+++ judoshiai/basic_defs.h
@@ -0,0 +1,16 @@
+#define STRING_SIZE        250     /* Size of strings          */
+#define VARNAME_SIZE       8       /* Maximum size of varnames */
+#define MAX_D_VARS         1000    /* Maximum variables        */
+#define MAX_STR_VARS       250     /* Maximum strings          */
+#define MAX_DIM_STR        50      /* Maximum DIM-ed strings   */
+#define MAX_DIM_VARS       50      /* Maximum DIM-ed vars      */
+#define STACK_SIZE         50      /* Stack sizes              */
+#define MAX_ON_LINES       25      /* ON .. jumplines          */
+#define B_TRUE             -1.0    
+#define B_FALSE            0.0     
+#define MAXLINES           10000   /* Maximum of linenumber    */
+#define MAX_FILES          25      /* Maximum of filesopen     */
+#define DEFAULT_OPEN_LEN   128     /* default record size      */
+#define DEF_UMASK          0660    /* default umask            */
+#define MAX_LINE_WIDTH     250     /* maximum chars on a line  */
+#define MAX_DIM            3       /* maximum dimensions       */
