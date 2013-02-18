ec06937718b4abc382ff94d186d064f66cf1d330
Hannu Jokinen <oh2ncp@kolumbus.fi>
2013-02-18 10:40:31 +0000

    Support for dynamic results language files.

--- judoshiai/print-texts.c
+++ judoshiai/print-texts.c
@@ -12,60 +12,75 @@
 
 #include "judoshiai.h"
 
-gchar *print_texts[numprinttexts][NUM_LANGS] = {
-    /* fi               se            en               es               et               uk               is               no               pl*/
-    {"Kilpailijat", "Tävlande", "Competitors", "Competidores", "", "Учасники", "Keppendur", "Deltakere", "Zawodnicy"},
-    {"Kilpailijat A", "Tävlande A", "Competitors A", "Competidores A", "", "Учасники A", "Keppendur A", "Deltakere A", "Zawodnicy A"},
-    {"Kilpailijat B", "Tävlande B", "Competitors B", "Competidores B", "", "Учасники B", "Keppendur B", "Deltakere B", "Zawodnicy B"},
-    {"Kilpailijat C", "Tävlande C", "Competitors C", "Competidores C", "", "Учасники C", "Keppendur C", "Deltakere C", "Zawodnicy C"},
-    {"Kilpailijat D", "Tävlande D", "Competitors D", "Competidores D", "", "Учасники D", "Keppendur D", "Deltakere D", "Zawodnicy D"},
-    {"N:o", "Nr", "#", "Nº", "", "№", "Nr", "Nr", "Nr"},
-    {"Nimi", "Namn", "Name", "Nombre", "", "Учасник", "Nafn", "Navn", "Imię"},
-    {"Vyö", "Grad", "Grade", "Kyu", "", "Розряд", "Gráða", "Grad", "Stopień"},
-    {"Seura", "Klubb", "Club", "Club", "", "Команда", "Félag", "Klubb", "Klub"},
-    {"Ottelut", "Matcherna", "Matches", "Combates", "", "Сутички", "Viðureignir", "Kamper", "Walki"},
-    {"Ottelut A", "Matcherna A", "Matches A", "Combates A", "", "Сутички A", "Viðureignir A", "Kamper A", "Walki A"},
-    {"Ottelut B", "Matcherna B", "Matches B", "Combates B", "", "Сутички B", "Viðureignir B", "Kamper B", "Walki B"},
-    {"Ottelut C", "Matcherna C", "Matches C", "Combates C", "", "Сутички C", "Viðureignir C", "Kamper C", "Walki C"},
-    {"Ottelut D", "Matcherna D", "Matches D", "Combates D", "", "Сутички D", "Viðureignir D", "Kamper D", "Walki D"},
 
-    {"Ottelu", "Match", "Match", "Comb.", "", "Сутичка", "Viðureign", "Kamp", "Walka"},
-    {"Sininen", "Blå", "Blue", "Azul", "", "Синій", "Blár", "Blå", "Niebieski"},
-    {"Valkoinen", "Vit", "White", "Blanco", "", "Білий", "Hvítur", "Hvit", "Biały"},
-    {"Tulos", "Resultat", "Result", "Resultado", "", "Результат", "Úrslit", "Resultat", "Wynik"},
-    {"Tulokset", "Resultat", "Results", "Clasificación", "", "Результати", "Úrslit", "Resultater", "Wyniki"},
-    {"Aika", "Tid", "Time", "Tiempo", "", "Час", "Tími", "Tid", "Czas"},
-    {"Voit.", "Vin.", "Wins", "Victs.", "", "П", "Vin.", "Seire", "Wygrywa"},
-    {"Pist.", "Poäng", "Pts", "Pts.", "", "О", "Stig", "Poeng", "Pkt."},
-    {"Sij.", "Plats", "Pos", "Pos.", "", "М", "Sæti", "Plass", "Miejsce"},
-    {"Ed. voit", "Föregående vinnare", "Previous winner", "Ganador anterior", "", "Previous winner", "Fyrri sigurvegari", "Forrige Vinner", "Poprzedni wygrany"},
+static gint num_print_lang = 0;
 
-    {"Valmistautuvat", "Därefter", "Next", "Siguiente", "", "Наступні", "Næsta", "Neste", "Następny"},
-    {"Paino:", "Vikt:", "Weight:", "Peso:", "", "Вага", "Þyngd", "Vekt", "Waga:"},
-    {"ALUSTAVA AIKATAULU", "PRELIMINÄR TIDTABELL", "PRELIMINARY SCHEDULE", "AGENDA PRELIMINAR", "", "ПОПЕРЕДНІЙ РОЗКЛАД", "Bráðabirgða áætlun", "FORELØPIG TIDSSKJEMA", "WSTĘPNY PLAN"},
-    {"punnituslaput.pdf", "viktlappor.pdf", "weighin-notes.pdf", "notas-pesaje.pdf", "weighin-notes.pdf", "weighin-notes.pdf","vigtunnar-listi", "veielapper.pdf", "waga-notatki.pdf"},
-    {"aikataulu.pdf", "tidtabell.pdf", "schedule.pdf", "agenda.pdf", "schedule.pdf", "schedule.pdf", "áætlun", "tidsskjema.pdf", "plan.pdf"},
-    {"sarjat.pdf", "viktklass.pdf", "categories.pdf", "categorias.pdf", "categories.pdf", "categories.pdf", "flokkar", "vektklasser.pdf", "kategorie.pdf"},
-    {"Sarja", "Viktklass", "Category", "Categoria", "", "Категорія", "Þyngdarflokkur", "Vektklasse", "Kategoria"},
-    {"Seuraavat ottelut", "Nästa match", "Next fights", "Siguiente combate", "", "Наступна", "Næstu viðureignir", "Neste kamp", "Kolejne walki"},
-    {"Sarjat", "Viktklass", "Categories", "Categorias", "", "Категорії", "Þyngdarflokkar", "Vektklasser", "Kategorie"},
-    {"Tilastot", "Statistik", "Statistics", "Estadísticas", "", "Статистика", "Tölfræði", "Statistikk", "Statystyki"},
+gchar *print_texts[numprinttexts][NUM_PRINT_LANGS];
+gchar *print_lang_menu_texts[NUM_PRINT_LANGS];
+gchar  print_lang_names[NUM_PRINT_LANGS][2];
 
-    {"Mitalit", "Medaljer", "Medals", "Medallas", "", "Медалі", "Verðlaun", "Medaljer", "Medale"},
-    {"Yhteensä", "Totalt", "Total", "Total", "", "Підсумок", "Samtals", "Total", "Suma"},
-    {"Maa", "Land", "Country", "País", "Riik", "Країна", "Land", "Land", "Kraj"},
+static void get_print_texts(gchar a, gchar b, gchar *filename)
+{
+    gint linenum = 0;
+    FILE *f = fopen(filename, "r");
+    if (!f)
+        return;
 
-    // Coach display texts
-    {"Ei arvottu", "Not drawn", "Not drawn", "Not drawn", "Not drawn", "Not drawn", "Not drawn", "Not drawn", "Not drawn"},
-    {"Loppunut", "Finished", "Finished", "Finished", "Finished", "Finished", "Finished", "Finished", "Finished"},
-    {"Ottelu käynnissä", "Match ongoing", "Match ongoing", "Match ongoing", "Match ongoing", "Match ongoing", "Match ongoing", "Match ongoing", "Match ongoing"},
-    {"Sarja alkanut", "Started", "Started", "Started", "Started", "Started", "Started", "Started", "Started"},
-    {"Arvonta valmis", "Drawing ready", "Drawing ready", "Drawing ready", "Drawing ready", "Drawing ready", "Drawing ready", "Drawing ready", "Drawing ready"},
-    {"Valmentaja", "Coach", "Coach", "Coach", "Coach", "Coach", "Coach", "Coach", "Coach"},
-    {"Sukunimi", "Surname", "Surname", "Surname", "Surname", "Surname", "Surname", "Surname", "Surname"},
-    {"Tilanne", "Status", "Status", "Status", "Status", "Status", "Status", "Status", "Status"},
-    {"näyttö", "Display", "Display", "Display", "Display", "Display", "Display", "Display", "Display"},
-    // Text: Match after n matches
-    {"Ottelu", "Match after", "Match after", "Match after", "Match after", "Match after", "Match after", "Match after", "Match after"},
-    {"ottelun jälkeen", "matches", "matches", "matches", "matches", "matches", "matches", "matches", "matches"},
-};
+    print_lang_names[num_print_lang][0] = a;
+    print_lang_names[num_print_lang][1] = b;
+
+    gchar line[256];
+
+    while (fgets(line, sizeof(line), f)) {
+        if (strncmp(line, "###", 3) == 0)
+            continue;
+
+        gchar *p1 = strchr(line, '\r');
+        if (p1)	*p1 = 0;
+        p1 = strchr(line, '\n');
+        if (p1)	*p1 = 0;
+
+        linenum++;
+
+        if (linenum == 1) {
+            print_lang_menu_texts[num_print_lang] = g_strdup(line);
+            continue;
+        }
+        
+        if (linenum-2 < numprinttexts)
+            print_texts[linenum-2][num_print_lang] = g_strdup(line);
+        else
+            g_print("Extra lines in %s\n", filename);
+    }
+
+    fclose(f);
+    num_print_lang++;
+}
+
+void init_print_texts(void)
+{
+    memset(&print_texts, 0, sizeof(print_texts));
+    memset(&print_lang_menu_texts, 0, sizeof(print_lang_menu_texts));
+    memset(&print_lang_names, 0, sizeof(print_lang_names));
+
+    gchar *dirname = g_build_filename(installation_dir, "etc", NULL);
+    GDir *dir = g_dir_open(dirname, 0, NULL);
+
+    if (!dir)
+        return;
+
+    const gchar *fname = g_dir_read_name(dir);
+    while (fname) {
+        if (strncmp(fname, "result-texts-", 13) == 0 && num_print_lang < NUM_PRINT_LANGS) {
+            gchar a = fname[13], b = fname[14];
+            gchar *fullname = g_build_filename(dirname, fname, NULL);
+            get_print_texts(a, b, fullname);
+            g_free(fullname);
+        }
+    
+        fname = g_dir_read_name(dir);
+    }
+
+    g_dir_close(dir);
+    g_free(dirname);
+}
