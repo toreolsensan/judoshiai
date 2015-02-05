a51efd5f9ee7c7cf351a0501670e23809fe206b3
Hannu Jokinen <oh2ncp@kolumbus.fi>
2015-02-05 12:52:59 +0200

    Updates.

--- judoshiai/custom-category.h
+++ judoshiai/custom-category.h
@@ -3,15 +3,22 @@
 
 /** Custom brackets */
 
+#ifndef NUM_COMPETITORS
+  #define NUM_COMPETITORS 128
+#endif
+
 #define NUM_CUSTOM_MATCHES   512
 #define NUM_CUST_POS          16
 #define NUM_ROUND_ROBIN_POOLS 16
 #define NUM_RR_MATCHES        64
+#define NUM_BEST_OF_3_PAIRS   16
+#define NUM_GROUPS             8
 
 typedef struct player_bare {
 #define COMP_TYPE_COMPETITOR  1
 #define COMP_TYPE_MATCH       2
 #define COMP_TYPE_ROUND_ROBIN 3
+#define COMP_TYPE_BEST_OF_3   4
     int    type;
     int    num; // competitor num or match num or rr num
     int    pos; // winner = 1, loser = 2, or rr position
@@ -31,9 +38,23 @@ typedef struct position_bare {
 } position_bare_t;
 
 typedef struct round_robin_bare {
+    char name[16];
     int rr_matches[NUM_RR_MATCHES];
     int num_rr_matches;
+    competitor_bare_t competitors[NUM_COMPETITORS];
+    int num_competitors;
 } round_robin_bare_t;
+
+typedef struct best_of_three_bare {
+    char name[16];
+    int matches[3];
+} best_of_three_bare_t;
+
+typedef struct group_bare {
+    int competitors[NUM_COMPETITORS];
+    int num_competitors;
+    int type; // 0 = knock out, 1 = round robin
+} group_bare_t;
 
 struct custom_data {
     char name_long[64];
@@ -43,13 +64,20 @@ struct custom_data {
     round_robin_bare_t round_robin_pools[NUM_ROUND_ROBIN_POOLS];
     int num_round_robin_pools;
 
+    best_of_three_bare_t best_of_three_pairs[NUM_BEST_OF_3_PAIRS];
+    int num_best_of_three_pairs;
+
     match_bare_t matches[NUM_CUSTOM_MATCHES];
     int num_matches;
 
     position_bare_t positions[NUM_CUST_POS];
     int num_positions;
+
+    group_bare_t groups[NUM_GROUPS];
+    int num_groups;
 };
 
-extern int read_custom_category(char *name, struct custom_data *data);
+extern char *read_custom_category(char *name, struct custom_data *data);
+extern int make_svg_file(int argc, char *argv[]);
 
 #endif
