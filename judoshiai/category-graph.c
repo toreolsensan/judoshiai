74bd0e4f10fd04155e9916f2e08e50fcecbc9886
Hannu Jokinen <oh2ncp@kolumbus.fi>
2016-07-14 13:44:18 +0300

    Also moved matches are shown. New menu to show brackets and move or unfreeze matches.

--- judoshiai/category-graph.c
+++ judoshiai/category-graph.c
@@ -42,6 +42,7 @@ static struct {
     double x1, y1, x2, y2;
     gint   index;
     gint   tatami;
+    gint   orig_tatami;
     gint   group;
 } point_click_areas[NUM_RECTANGLES];
 
@@ -78,15 +79,29 @@ void draw_gategory_graph(void)
     refresh_darea();
 }
 
+#define set_left_right(_t) do {						\
+    left = (mirror_display && _t) ?					\
+	(number_of_tatamis - _t + 1)*colwidth :				\
+	(_t)*colwidth;						\
+    right = (mirror_display && _t) ?					\
+	(number_of_tatamis - _t + 2)*colwidth :				\
+	(_t+1)*colwidth;						\
+    } while (0)
+
+
 static void paint(cairo_t *c, gdouble paper_width, gdouble paper_height, gpointer userdata)
 {
-    gint i, matches_left, matches_time;
+    gint i, matches_left[NUM_TATAMIS+1], matches_time[NUM_TATAMIS+1], tatami;
     cairo_text_extents_t extents;
-    gdouble       y_pos = 0.0, colwidth = W(1.0/(number_of_tatamis + 1));
-    gint          max_group;
+    gdouble       y_pos[NUM_TATAMIS+1], colwidth = W(1.0/(number_of_tatamis + 1));
+    gint          max_group[NUM_TATAMIS+1];
     struct tm    *tm;
     time_t        secs;
     cairo_surface_t *cs = userdata;
+    struct category_data *catdatas[NUM_TATAMIS];
+    gint old_group = 1000000;
+    gdouble left, right;
+    gchar buf[32];
 
     if (!anchor)
         anchor = get_image("anchor.png");
@@ -109,191 +124,238 @@ static void paint(cairo_t *c, gdouble paper_width, gdouble paper_height, gpointe
     cairo_set_line_width(c, THIN_LINE);
     cairo_set_source_rgb(c, 0.0, 0.0, 0.0);
 
-    y_pos = extents.height*1.4;
-
     for (i = 0; i <= number_of_tatamis; i++) {
-        gdouble left = (i)*colwidth;
-        gdouble right = (i+1)*colwidth;
-        gchar buf[30];
-        gint old_group = 0;
-        struct category_data *catdata = category_queue[i].next;
+	catdatas[i] = category_queue[i].next;
+	y_pos[i] = extents.height*1.4;
+        matches_left[i] = 0;
+        matches_time[i] = 0;
+        max_group[i] = 0;
+    }
 
-        if (mirror_display && i) {
-            left = (number_of_tatamis - i + 1)*colwidth;
-            right = (number_of_tatamis - i + 2)*colwidth;
-        }
+    while (1) {
+	gint grp_found = 0;
+	old_group = 1000000;
 
-        matches_left = 0;
-        matches_time = 0;
-        max_group = 0;
+	/* find min group */
+	for (i = 0; i <= number_of_tatamis; i++)
+	    if (catdatas[i] && catdatas[i]->group < old_group)
+		old_group = catdatas[i]->group;
 
-        y_pos = extents.height*1.4;
+	for (tatami = 0; tatami <= number_of_tatamis; tatami++) {
+	    struct category_data *catdata = catdatas[tatami];
+	    gboolean group_on_tatami = FALSE;
 
-        cairo_set_line_width(c, THIN_LINE);
-        cairo_set_source_rgb(c, 0.0, 0.0, 0.0);
+	    while (catdata && catdata->group == old_group) {
+		gint n = 1;
+		gdouble x;
+		gint mt = get_category_match_time(catdata->category);
+		if (mt < 180) mt = 180;
+		grp_found++;
+		group_on_tatami = TRUE;
+		if (old_group > max_group[tatami])
+		    max_group[tatami] = old_group;
 
-        if (catdata)
-            old_group = catdata->group;
+		cairo_set_line_width(c, THIN_LINE);
+		cairo_set_source_rgb(c, 0.0, 0.0, 0.0);
 
-        while (catdata) {
-            gint n = 1;
-            gdouble x;
+		set_left_right(tatami);
 
-            gint mt = get_category_match_time(catdata->category);
-            if (mt < 180)
-                mt = 180;
+		x = n = 0;
+		if (catdata->by_tatami[0].match_count > 0)
+		    x = catdata->by_tatami[0].matched_matches_count*colwidth /
+			catdata->by_tatami[0].match_count;
 
-            if (catdata->group != old_group) {
-                cairo_save(c);
-                cairo_set_line_width(c, THICK_LINE);
-                cairo_set_source_rgb(c, 0.0, 0.0, 1.0);
-                cairo_move_to(c, left, y_pos);
-                cairo_line_to(c, right, y_pos);
-                cairo_stroke(c);
+		/* find total number of matches, even w/o draw */
+		if (catdata->match_status & REAL_MATCH_EXISTS) {
+		    if ((catdata->match_status & MATCH_UNMATCHED))
+			n = catdata->by_tatami[0].match_count -
+			    catdata->by_tatami[0].matched_matches_count;
+		} else {
+		    GtkTreeIter tmp_iter;
+		    if (find_iter(&tmp_iter, catdata->index)) {
+			gint k = gtk_tree_model_iter_n_children(current_model, &tmp_iter);
+			n = num_matches_left(catdata->index, k);
+		    }
+		}
 
-                if (matches_left) {
-                    /* write time */
-                    cairo_rectangle(c, right - extents.width,
-                                    y_pos - extents.height,
-                                    extents.width, extents.height);
-                    cairo_fill(c);
+		if (n == 0)
+		    goto loop;
 
-                    secs = time(NULL) + matches_time;
-                    tm = localtime(&secs);
+		cairo_save(c);
+		cairo_set_source_rgb(c, 0.0, 1.0, 0.0);
+		cairo_rectangle(c, left, y_pos[tatami], x, BOX_HEIGHT);
+		cairo_fill(c);
 
-                    cairo_set_source_rgb(c, 1.0, 1.0, 1.0);
-                    sprintf(buf, "%02d:%02d", tm->tm_hour, tm->tm_min);
-                    cairo_move_to(c, right - extents.width, y_pos);
-                    cairo_show_text(c, buf);
-                }
+		if (catdata->match_count == 0)
+		    cairo_set_source_rgb(c, 1.0, 1.0, 1.0);
+		else if (catdata->matched_matches_count == 0)
+		    cairo_set_source_rgb(c, 1.0, 1.0, 0.0);
+		else
+		    cairo_set_source_rgb(c, 1.0, 0.65, 0.0);
 
-                cairo_restore(c);
+		cairo_rectangle(c, left+x, y_pos[tatami], colwidth - x, BOX_HEIGHT);
+		cairo_fill(c);
 
-                y_pos += 1.0;
-                old_group = catdata->group;
-            }
+		if (cannot_move_category(catdata->index)) {
+		    cairo_set_source_surface(c, anchor, left + colwidth/2, y_pos[tatami]);
+		    cairo_paint(c);
+		}
 
-            if (catdata->group > max_group)
-                max_group = catdata->group;
+		cairo_restore(c);
 
-            x = n = 0;
+		/* category name and time */
+		gint mul = 1;
+		if (catdata->deleted & TEAM_EVENT) {
+		    mul = find_num_weight_classes(catdata->category);
+		    if (mul == 0) mul = 1;
+		}
 
-            if (catdata->match_count > 0) {
-                x = catdata->matched_matches_count*colwidth/catdata->match_count;
-                //n = catdata->match_count - catdata->matched_matches_count;
-            }
+		matches_left[tatami] += n*mul;//catdata->match_count - catdata->matched_matches_count;
+		matches_time[tatami] += n*mt*mul;
 
-            GtkTreeIter tmp_iter;
-            if (find_iter(&tmp_iter, catdata->index)) {
-                gint k = gtk_tree_model_iter_n_children(current_model, &tmp_iter);
-                n = num_matches_left(catdata->index, k);
-            }
+		cairo_move_to(c, left+5, y_pos[tatami]+extents.height);
+		cairo_show_text(c, catdata->category);
+#if 0
+		struct judoka *j = get_data(catdata->index);
+		if (j) {
 
-            if (n == 0)
-                goto loop;
+		    cairo_move_to(c, left+5, y_pos[tatami]+extents.height);
+		    cairo_show_text(c, j->last);
+		    free_judoka(j);
+		}
+#endif
 
-            cairo_save(c);
-            cairo_set_source_rgb(c, 0.0, 1.0, 0.0);
-            cairo_rectangle(c, left, y_pos, x, BOX_HEIGHT);
-            cairo_fill(c);
+		/* point click areas */
+		point_click_areas[num_rectangles].index = catdata->index;
+		point_click_areas[num_rectangles].group = catdata->group;
+		point_click_areas[num_rectangles].tatami = tatami;
+		point_click_areas[num_rectangles].orig_tatami = 0;
+		point_click_areas[num_rectangles].x1 = left;
+		point_click_areas[num_rectangles].y1 = y_pos[tatami];
+		point_click_areas[num_rectangles].x2 = right;
+		y_pos[tatami] += BOX_HEIGHT;
+		point_click_areas[num_rectangles].y2 = y_pos[tatami];
+		if (num_rectangles < NUM_RECTANGLES-1)
+		    num_rectangles++;
 
-            if (catdata->match_count == 0)
-                cairo_set_source_rgb(c, 1.0, 1.0, 1.0);
-            else if (catdata->matched_matches_count == 0)
-                cairo_set_source_rgb(c, 1.0, 1.0, 0.0);
-            else
-                cairo_set_source_rgb(c, 1.0, 0.65, 0.0);
+		cairo_move_to(c, left, y_pos[tatami]);
+		cairo_line_to(c, right, y_pos[tatami]);
+		cairo_stroke(c);
 
-            cairo_rectangle(c, left+x, y_pos, colwidth - x, BOX_HEIGHT);
-            cairo_fill(c);
+	    loop:
+		/* Matches moved to other tatamis. */
+		for (i = 1; i <= number_of_tatamis; i++) {
+		    n = catdata->by_tatami[i].match_count -
+			catdata->by_tatami[i].matched_matches_count;
+		    if (n <= 0)
+			continue;
 
-            if (cannot_move_category(catdata->index)) {
-                cairo_set_source_surface(c, anchor, left + colwidth/2, y_pos);
-                cairo_paint(c);
-            }
+		    set_left_right(i);
+		    matches_left[i] += n*mul;
+		    matches_time[i] += n*mt*mul;
+		    x = catdata->by_tatami[i].matched_matches_count*colwidth /
+			catdata->by_tatami[i].match_count;
 
-            cairo_restore(c);
+		    cairo_save(c);
+		    cairo_set_source_rgb(c, 0.5, 1.0, 0.5);
+		    cairo_rectangle(c, left, y_pos[i], x, BOX_HEIGHT);
+		    cairo_fill(c);
 
-            gint mul = 1;
-            if (catdata->deleted & TEAM_EVENT) {
-                mul = find_num_weight_classes(catdata->category);
-                if (mul == 0) mul = 1;
-            }
+		    if (catdata->by_tatami[i].matched_matches_count == 0)
+			cairo_set_source_rgb(c, 1.0, 1.0, 0.5);
+		    else
+			cairo_set_source_rgb(c, 1.0, 0.75, 0.5);
 
-            matches_left += n*mul;//catdata->match_count - catdata->matched_matches_count;
-            matches_time += n*mt*mul;
+		    cairo_rectangle(c, left+x, y_pos[i], colwidth - x, BOX_HEIGHT);
+		    cairo_fill(c);
 
-            struct judoka *j = get_data(catdata->index);
-            if (j) {
-                cairo_move_to(c, left+5, y_pos+extents.height);
-                cairo_show_text(c, j->last);
-                free_judoka(j);
-            }
+		    cairo_restore(c);
 
-            point_click_areas[num_rectangles].index = catdata->index;
-            point_click_areas[num_rectangles].group = catdata->group;
-            point_click_areas[num_rectangles].tatami = i;
-            point_click_areas[num_rectangles].x1 = left;
-            point_click_areas[num_rectangles].y1 = y_pos;
-            point_click_areas[num_rectangles].x2 = right;
-            y_pos += BOX_HEIGHT;
-            point_click_areas[num_rectangles].y2 = y_pos;
-            if (num_rectangles < NUM_RECTANGLES-1)
-                num_rectangles++;
+		    snprintf(buf, sizeof(buf), "%s (T%d)", catdata->category, tatami);
+		    cairo_move_to(c, left+5, y_pos[i]+extents.height);
+		    cairo_show_text(c, buf);
 
-            cairo_move_to(c, left, y_pos);
-            cairo_line_to(c, right, y_pos);
-            cairo_stroke(c);
+		    /* point click areas */
+		    point_click_areas[num_rectangles].index = catdata->index;
+		    point_click_areas[num_rectangles].group = catdata->group;
+		    point_click_areas[num_rectangles].tatami = i;
+		    point_click_areas[num_rectangles].orig_tatami = tatami;
+		    point_click_areas[num_rectangles].x1 = left;
+		    point_click_areas[num_rectangles].y1 = y_pos[i];
+		    point_click_areas[num_rectangles].x2 = right;
+		    y_pos[i] += BOX_HEIGHT;
+		    point_click_areas[num_rectangles].y2 = y_pos[i];
+		    if (num_rectangles < NUM_RECTANGLES-1)
+			num_rectangles++;
 
+		    cairo_move_to(c, left, y_pos[i]);
+		    cairo_line_to(c, right, y_pos[i]);
+		    cairo_stroke(c);
+		} /* for i */
 
-        loop:
-            catdata = catdata->next;
-        } // while cat queue
+		catdatas[tatami] = catdata = catdata->next;
+	    } /* while group */
 
-        cairo_save(c);
-        cairo_set_line_width(c, THICK_LINE);
-        cairo_set_source_rgb(c, 0.0, 0.0, 1.0);
-        cairo_move_to(c, left, y_pos);
-        cairo_line_to(c, right, y_pos);
-        cairo_stroke(c);
+	    set_left_right(tatami);
 
-        if (matches_left) {
-            /* write time */
-            cairo_rectangle(c, right - extents.width,
-                            y_pos - extents.height,
-                            extents.width, extents.height);
-            cairo_fill(c);
+	    /* draw horizontal line after group */
+	    if (group_on_tatami) {
+		cairo_save(c);
 
-            secs = time(NULL) + matches_time;
-            tm = localtime(&secs);
+		cairo_set_line_width(c, THICK_LINE);
+		cairo_set_source_rgb(c, 0.0, 0.0, 1.0);
+		cairo_move_to(c, left, y_pos[tatami]);
+		cairo_line_to(c, right, y_pos[tatami]);
+		cairo_stroke(c);
 
-            cairo_set_source_rgb(c, 1.0, 1.0, 1.0);
-            sprintf(buf, "%02d:%02d", tm->tm_hour, tm->tm_min);
-            cairo_move_to(c, right - extents.width, y_pos);
-            cairo_show_text(c, buf);
-        }
+		if (matches_left[tatami]) {
+		    /* write time */
+		    cairo_rectangle(c, right - extents.width,
+				    y_pos[tatami] - extents.height,
+				    extents.width, extents.height);
+		    cairo_fill(c);
 
-        cairo_restore(c);
-        y_pos += 1.0;
+		    secs = time(NULL) + matches_time[tatami];
+		    tm = localtime(&secs);
+
+		    cairo_set_source_rgb(c, 1.0, 1.0, 1.0);
+		    sprintf(buf, "%02d:%02d", tm->tm_hour, tm->tm_min);
+		    cairo_move_to(c, right - extents.width, y_pos[tatami]);
+		    cairo_show_text(c, buf);
+		}
+
+		cairo_restore(c);
+	    }
+
+	    y_pos[tatami] += 1.0;
+	} /* for tatami */
+
+	if (!grp_found)
+	    break;
+    } /* while 1 */
+
+    for (tatami = 0; tatami <= number_of_tatamis; tatami++) {
+	set_left_right(tatami);
 
         cairo_move_to(c, 10 + left, extents.height);
         if (i == 0)
-            sprintf(buf, "%s  [%d]", _("Unlocated"), matches_left);
+            sprintf(buf, "%s  [%d]", _("Unlocated"), matches_left[tatami]);
         else
-            sprintf(buf, "Tatami %d  [%d]", i, matches_left);
+            sprintf(buf, "Tatami %d  [%d]", tatami, matches_left[tatami]);
         cairo_show_text(c, buf);
 
         point_click_areas[num_rectangles].index = 0;
-        point_click_areas[num_rectangles].group = max_group+1;
-        point_click_areas[num_rectangles].tatami = i;
+        point_click_areas[num_rectangles].group = max_group[tatami]+1;
+        point_click_areas[num_rectangles].tatami = tatami;
+        point_click_areas[num_rectangles].orig_tatami = 0;
         point_click_areas[num_rectangles].x1 = left;
-        point_click_areas[num_rectangles].y1 = y_pos;
+        point_click_areas[num_rectangles].y1 = y_pos[tatami];
         point_click_areas[num_rectangles].x2 = right;
         point_click_areas[num_rectangles].y2 = H(1.0);
         if (num_rectangles < NUM_RECTANGLES-1)
             num_rectangles++;
 
-        if (i) {
+        if (tatami) {
             cairo_save(c);
             cairo_set_line_width(c, THICK_LINE);
             cairo_set_source_rgb(c, 0.0, 0.0, 0.0);
@@ -342,8 +404,6 @@ static gboolean expose_scrolled(GtkWidget *widget, GdkEventExpose *event, gpoint
 /* This is called when we need to draw the windows contents */
 static gboolean expose(GtkWidget *widget, GdkEventExpose *event, gpointer userdata)
 {
-    //static gint cnt = 0;
-    //g_print("CATEGORY GRAPH: expose %d\n", cnt++);
 #if (GTKVER == 3)
     cairo_t *c = (cairo_t *)event;
     paint(c, gtk_widget_get_allocated_width(widget), gtk_widget_get_allocated_height(widget), NULL);
@@ -496,6 +556,24 @@ static gint find_box(gdouble x, gdouble y)
     return -1;
 }
 
+#define AREA_SHIFT        4
+#define ARG_MASK        0xf
+
+static void handle_menu(GtkWidget *menuitem, gpointer userdata)
+{
+    gint t = (ptr_to_gint(userdata)) >> AREA_SHIFT;
+    gint arg = (ptr_to_gint(userdata)) & ARG_MASK;
+
+    if (arg == UNFREEZE_EXPORTED ||
+	arg == UNFREEZE_IMPORTED) {
+	db_freeze_matches(point_click_areas[t].tatami, 0, 0, arg);
+    } else if (arg == UNFREEZE_THESE) {
+	db_freeze_matches(point_click_areas[t].tatami, point_click_areas[t].index, 0, arg);
+    } else if (arg == MOVE_MATCHES) {
+	view_popup_menu_move_matches(NULL, gint_to_ptr(point_click_areas[t].index));
+    }
+}
+
 static gboolean mouse_click(GtkWidget *sheet_page,
 			    GdkEventButton *event,
 			    gpointer userdata)
@@ -507,37 +585,82 @@ static gboolean mouse_click(GtkWidget *sheet_page,
 #endif
     button_drag = FALSE;
 
-    /* single click with the right mouse button? */
-    if (event->type == GDK_BUTTON_PRESS &&
-        (event->button == 1 || event->button == 3)) {
+    if (event->type == GDK_BUTTON_PRESS) {
         gdouble x = event->x, y = event->y;
         gint t;
-
-        dragged_x = x;
-        dragged_y = y;
 
         t = find_box(x, y);
         if (t < 0)
             return FALSE;
 
-        if (cannot_move_category(point_click_areas[t].index))
-            return FALSE;
+        if (event->button == 1) {
+	    if (cannot_move_category(point_click_areas[t].index))
+		return FALSE;
 
-        button_drag = TRUE;
-        dragged_cat = point_click_areas[t].index;
-        if (dragged_cat < 10000)
-            button_drag = FALSE;
-        start_box = t;
+	    dragged_x = x;
+	    dragged_y = y;
+	    button_drag = TRUE;
+	    dragged_cat = point_click_areas[t].index;
+	    if (dragged_cat < 10000)
+		button_drag = FALSE;
+	    start_box = t;
 
-        struct category_data *catdata = avl_get_category(dragged_cat);
-        if (catdata)
-            snprintf(dragged_text, sizeof(dragged_text), "%s -> ?",
-                     catdata->category);
+	    struct category_data *catdata = avl_get_category(dragged_cat);
+	    if (catdata)
+		snprintf(dragged_text, sizeof(dragged_text), "%s -> ?",
+			 catdata->category);
 
-        refresh_window();
+	    refresh_window();
+	    return TRUE;
+        } else if (event->button == 3) {
+	    GtkWidget *menu, *menuitem;
+	    gpointer p;
 
-        return TRUE;
+	    menu = gtk_menu_new();
 
+	    menuitem = gtk_menu_item_new_with_label(_("Unfreeze exported"));
+	    p = gint_to_ptr(UNFREEZE_EXPORTED | (t << AREA_SHIFT));
+	    g_signal_connect(menuitem, "activate",
+			     (GCallback) handle_menu, p);
+	    gtk_menu_shell_append(GTK_MENU_SHELL(menu), menuitem);
+
+	    menuitem = gtk_menu_item_new_with_label(_("Unfreeze imported"));
+	    p = gint_to_ptr(UNFREEZE_IMPORTED | (t << AREA_SHIFT));
+	    g_signal_connect(menuitem, "activate",
+			     (GCallback) handle_menu, p);
+	    gtk_menu_shell_append(GTK_MENU_SHELL(menu), menuitem);
+
+	    if (point_click_areas[t].orig_tatami) {
+		menuitem = gtk_menu_item_new_with_label(_("Unfreeze this"));
+		p = gint_to_ptr(UNFREEZE_THESE | (t << AREA_SHIFT));
+		g_signal_connect(menuitem, "activate",
+				 (GCallback) handle_menu, p);
+		gtk_menu_shell_append(GTK_MENU_SHELL(menu), menuitem);
+	    }
+
+	    menuitem = gtk_menu_item_new_with_label(_("Move Matches"));
+	    p = gint_to_ptr(MOVE_MATCHES | (t << AREA_SHIFT));
+	    g_signal_connect(menuitem, "activate",
+			     (GCallback) handle_menu, p);
+	    gtk_menu_shell_append(GTK_MENU_SHELL(menu), menuitem);
+
+	    if (gint_to_ptr(point_click_areas[t].index) >= 10000) {
+		menuitem = gtk_menu_item_new_with_label(_("Show Sheet"));
+		g_signal_connect(menuitem, "activate",
+				 (GCallback) show_category_window,
+				 gint_to_ptr(point_click_areas[t].index));
+		gtk_menu_shell_append(GTK_MENU_SHELL(menu), menuitem);
+	    }
+
+	    gtk_widget_show_all(menu);
+
+	    gtk_menu_popup(GTK_MENU(menu), NULL, NULL, NULL, NULL,
+			   (event != NULL) ? event->button : 0,
+			   gdk_event_get_time((GdkEvent*)event));
+
+	    refresh_window();
+	    return TRUE;
+	}
     }
     return FALSE;
 }
